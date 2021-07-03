e1775caef07ce2607385306b05af063b103d2cdd
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2021-07-03 11:40:38 -0300

    Aula 02 - Maximum Speed

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
+import 'reflect-metadata';
 import express from 'express';
+
+import { router } from './routes';
+
+import './database';
 
 const app = express();
 
-app.get('/teste', (req, res) => {
-  return res.send('Olá NLW!');
-});
+app.use(express.json());
 
-app.post('/teste-post', (req, res) => {
-  return res.send('Olá NLW - Método POST!');
-});
+app.use(router);
 
 app.listen(3000, () => console.log('Server is running...'));
